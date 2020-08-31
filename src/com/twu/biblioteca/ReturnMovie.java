1a1de3c16ecdc8cbd6545dbd91f16e2c0a484eff
Aditi Sherekar <Aditi@Aditis-MacBook-Pro.local>
2020-08-31 16:09:15 +1200

    Added process movie return to return movie class

--- src/com/twu/biblioteca/ReturnMovie.java
+++ src/com/twu/biblioteca/ReturnMovie.java
@@ -1,6 +1,11 @@
 package com.twu.biblioteca;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class ReturnMovie implements ExcuteOptions {
+
+
 
     @Override
     public String displayOptionName() {
@@ -17,8 +22,37 @@ public class ReturnMovie implements ExcuteOptions {
         }
         else{
 
+            System.out.println("Please enter the movie you wish to return:");
+            Scanner optionSelection = new Scanner(System.in);
+            String selectedOption = optionSelection.nextLine();
+
+            processMovieReturn(selectedOption);
+
         }
 
+    }
+
+    public static void processMovieReturn(String optionSelected) {
+
+        Iterator<Movie> checkOutMovieIterator = Library.CheckedOutMovie().iterator();
+
+        while(checkOutMovieIterator.hasNext()){
+            Movie movie = checkOutMovieIterator.next();
+            if (movie.getMovieTitle().matches(optionSelected)) {
+                Library.MovieLibrary().add(movie);
+                Library.CheckedOutMovie().remove(movie);
+                System.out.println("Thank you for returning the movie.");
+
+            }
+            else if (!movie.getMovieTitle().matches(optionSelected) && checkOutMovieIterator.hasNext()) {
+
+                continue;
+            }
+            else{
+                System.out.println("That movie is not valid to return.");
+            }
+
+        }
 
     }
 }
