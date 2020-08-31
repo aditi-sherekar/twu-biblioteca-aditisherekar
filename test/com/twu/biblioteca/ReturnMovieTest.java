6372d353bd971f952af4ac868f1404fb9c2835d7
Aditi Sherekar <Aditi@Aditis-MacBook-Pro.local>
2020-08-31 15:52:37 +1200

    Added test to see if successful message is printed on returning correct movie

--- test/com/twu/biblioteca/ReturnMovieTest.java
+++ test/com/twu/biblioteca/ReturnMovieTest.java
@@ -36,5 +36,18 @@ public class ReturnMovieTest {
         assertEquals("There are no movies to be returned.\n", outContent.toString());
     }
 
+    @Test
+    public void doesReturnMovieGiveSuccessfulReturnMessage(){
+
+        Integer displayNameIndex = Menu.createOptionsList().indexOf(new ReturnMovie());
+
+        Movie movie1 = new Movie(new MovieTitle("Black Panther"), new Director("Ryan Coogler"), new MovieYear("2018"));
+        Library.checkedOutMovieList.add(movie1);
+        String input = "Black Panther";
+        ReturnMovie.processMovieReturn(input);
+
+        assertEquals("Thank you for returning the movie.\n", outContent.toString());
+    }
+
 
 }
